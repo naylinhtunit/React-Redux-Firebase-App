58457c0e47f7d6c28d224a58ae54407469b05116
naylinhtunit <naylin.htun@yathar.com>
2020-07-03 17:41:21 +0630

    signIn signUp permission

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signOut } from '../../store/actions/authActions';
 
-const SignedInLinks = () => {
+const SignedInLinks = (props) => {
 	return(
 			<ul className="right">
 				<li>
 					<NavLink to="/add">New project</NavLink>
 				</li>
 				<li>
-					<NavLink to="/">Log out</NavLink>
+					<a onClick={ props.signOut }>Log out</a>
 				</li>
 				<li>
 					<NavLink to="/" className="btn btn-floating pink lighten-1">MM</NavLink>
@@ -17,4 +19,10 @@ const SignedInLinks = () => {
 		)
 }
 
-export default SignedInLinks
+const mapDispatchToProps = (dispatch) => {
+	return {
+	  signOut: () => dispatch(signOut())
+	}
+  }
+
+export default connect(null, mapDispatchToProps)(SignedInLinks)
--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import thunk from 'redux-thunk';
 import { createFirestoreInstance, getFirestore, reduxFirestore } from 'redux-firestore';
-import { reactReduxFirebase, getFirebase, ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { reactReduxFirebase, getFirebase, ReactReduxFirebaseProvider, isLoaded } from 'react-redux-firebase';
 import fbConfig from './config/fbConfig';
 import firebase from 'firebase/app';
 
@@ -27,18 +27,22 @@ const store = createStore(
     createFirestoreInstance
   };
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ReactReduxFirebaseProvider {...rrfProps}>
-        <App />
-      </ReactReduxFirebaseProvider>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  function AuthIsLoaded({ children }) {
+    const auth = useSelector(state => state.firebase.auth)
+    if (!isLoaded(auth)) return <div>Loading Screen...</div>;
+        return children
+}
+  
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+          <AuthIsLoaded>
+            <App />
+          </AuthIsLoaded>
+        </ReactReduxFirebaseProvider>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  serviceWorker.unregister();
